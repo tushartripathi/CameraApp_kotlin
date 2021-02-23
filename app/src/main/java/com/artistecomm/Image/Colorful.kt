1a18423c8f942936b196b3c956c23a083d3f789c
tushar <tushartripathi301997@gmail.com>
2021-02-23 15:41:13 +0530

    Manuplating image pixels using seekbar

--- app/src/main/java/com/artistecomm/Image/Colorful.kt
+++ app/src/main/java/com/artistecomm/Image/Colorful.kt
@@ -3,6 +3,7 @@ package com.artistecomm.Image
 import android.graphics.Bitmap
 import android.graphics.Color
 import android.os.Build
+import android.widget.SeekBar
 import androidx.annotation.RequiresApi
 
 class Colorful(bitmap:Bitmap, redvalue:Float,greenvalue:Float,bluevalue:Float)
@@ -10,33 +11,49 @@ class Colorful(bitmap:Bitmap, redvalue:Float,greenvalue:Float,bluevalue:Float)
     private var bitmap: Bitmap
 
     private var redColorValue:Float=0f
-        get() = field
-        set(value) {
+
+        fun getRedColorValue():Float
+        {
+            return redColorValue
+        }
+
+        fun setRedColor(value:Float)
+        {
             if(value>=0 && value<=1)
-                field = value
+                redColorValue = value
         }
     private var greenColorValue:Float=0f
-        get() = field
-        set(value) {
-            if(value>=0 && value<=1)
-                field = value
-        }
+    fun getGreenColorValue():Float
+    {
+        return greenColorValue
+    }
+
+    fun setGreenColor(value:Float)
+    {
+        if(value>=0 && value<=1)
+            greenColorValue = value
+    }
     private var blueColorValue:Float=0f
-        get() = field
-        set(value) {
-            if(value>=0 && value<=1)
-                field = value
-        }
+    fun getBlueColorValue():Float
+    {
+        return blueColorValue
+    }
+
+    fun setBlueColor(value:Float)
+    {
+        if(value>=0 && value<=1)
+            blueColorValue = value
+    }
 
     init{
         this.bitmap = bitmap
-        redColorValue=redvalue
-        blueColorValue=bluevalue
-        greenColorValue=greenvalue
+         setRedColor(redvalue)
+         setBlueColor(bluevalue)
+         setGreenColor(greenvalue)
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    public fun getBitmap():Bitmap
+    public fun returnBitmap():Bitmap
     {
         var width = bitmap.getWidth()
         var height = bitmap.getHeight()
@@ -52,10 +69,10 @@ class Colorful(bitmap:Bitmap, redvalue:Float,greenvalue:Float,bluevalue:Float)
             while(col<height)
             {
                 var pixel = bitmap.getPixel(row,col)
-                pixel = Color.argb(Color.alpha(pixel) as Float,
-                        redColorValue * Color.red(pixel),
-                        greenColorValue * Color.green(pixel),
-                        blueColorValue * Color.blue(pixel))
+                pixel = Color.argb(Color.alpha(pixel) *1f,
+                        getRedColorValue() * Color.red(pixel),
+                        getGreenColorValue() * Color.green(pixel),
+                        getBlueColorValue() * Color.blue(pixel))
                 localBitmap.setPixel(row,col,pixel)
                 col++
             }
@@ -64,6 +81,5 @@ class Colorful(bitmap:Bitmap, redvalue:Float,greenvalue:Float,bluevalue:Float)
 
         return localBitmap
     }
-
 
 }
--- app/src/main/java/com/artistecomm/cameraapp/MainActivity.kt
+++ app/src/main/java/com/artistecomm/cameraapp/MainActivity.kt
@@ -3,15 +3,21 @@ package com.artistecomm.cameraapp
 import android.content.Intent
 import android.graphics.Bitmap
 import android.media.Image
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
 import android.widget.*
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
+import com.artistecomm.Image.Colorful
 import com.artistecomm.SytemTask.Permission
+import java.security.AccessController.getContext
 
-class MainActivity : AppCompatActivity() , View.OnClickListener{
+class MainActivity : AppCompatActivity() , View.OnClickListener {
+
+    lateinit var colourful: Colorful
 
     private lateinit var takeImageBtn: Button
     private lateinit var saveImageBtn: Button
@@ -22,8 +28,6 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
     private lateinit var redText: TextView
     private lateinit var greenText: TextView
     private lateinit var blueText: TextView
-
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,39 +45,69 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
         blueText = findViewById(R.id.textView3)
         takeImageBtn.setOnClickListener(this)
         saveImageBtn.setOnClickListener(this)
+        var customizeHandler: CustomizeHandler = CustomizeHandler()
+        redSeek.setOnSeekBarChangeListener(customizeHandler)
+        greenSeek.setOnSeekBarChangeListener(customizeHandler)
+        blueSeek.setOnSeekBarChangeListener(customizeHandler)
 
     }
 
     override fun onClick(view: View?) {
 
-        if(view != null)
-        if(view.getId()==R.id.buttonTakeIamge)
-        {
-           if(Permission.getPermission(this))
-            {
-                var camerIntent : Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                startActivityForResult(camerIntent, 1000)
-            }
-            else
-                Toast.makeText(this,"No Camera",Toast.LENGTH_SHORT).show()
-        }
-        else if(view.getId()==R.id.buttonSaveTheGallery)
-        {
+        if (view != null)
+            if (view.getId() == R.id.buttonTakeIamge) {
+                if (Permission.getPermission(this)) {
+                    var camerIntent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    startActivityForResult(camerIntent, 1000)
+                } else
+                    Toast.makeText(this, "No Camera", Toast.LENGTH_SHORT).show()
+            } else if (view.getId() == R.id.buttonSaveTheGallery) {
 
-        }
+            }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        Toast.makeText(this,"Got result",Toast.LENGTH_SHORT).show()
-        if(requestCode==1000 && resultCode==RESULT_OK)
-        {
+        Toast.makeText(this, "Got result", Toast.LENGTH_SHORT).show()
+        if (requestCode == 1000 && resultCode == RESULT_OK) {
             var bundle = data?.getExtras()
-            var bitmap:Bitmap= bundle?.get("data") as Bitmap
+            var bitmap: Bitmap = bundle?.get("data") as Bitmap
+            colourful = Colorful(bitmap, 0.0f, 0.0f, 0.0f)
+
             imageView?.setImageBitmap(bitmap)
         }
     }
 
-    
+    inner class CustomizeHandler : SeekBar.OnSeekBarChangeListener {
+        @RequiresApi(Build.VERSION_CODES.O)
+        override fun onProgressChanged(seekbar: SeekBar?, progress: Int, fromBool: Boolean) {
+            if (fromBool) {
+                if (R.id.seekBar == seekbar?.getId()) {
+                    colourful.setRedColor((progress / 100f) )
+                    redText.setText((progress / 100f ).toString())
+                }
+                if (R.id.seekBar2 == seekbar?.getId()) {
+                    colourful.setGreenColor((progress / 100f))
+                    greenText.setText((progress / 100f ).toString())
+                }
+                if (R.id.seekBar3 == seekbar?.getId()) {
+                    colourful.setBlueColor((progress / 100f) )
+                    blueText.setText((progress / 100f ).toString())
+                }
+            }
+            var bitmap = colourful.returnBitmap()
+            imageView.setImageBitmap(bitmap)
+        }
+
+        override fun onStartTrackingTouch(p0: SeekBar?) {
+            var a=1
+        }
+
+        override fun onStopTrackingTouch(p0: SeekBar?) {
+            var b=1
+        }
+
+
+    }
 }
